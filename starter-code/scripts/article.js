8b1cdda369abc33f28ecc692ca459ee700a98820
Scott <sdmccoy@gmail.com>
2017-05-15 15:51:22 -0700

    retrieved the data, appended to html, set it to local storage, then loaded the page

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -50,31 +50,29 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
+    //TODO: What do we pass in to loadAll()?
+    Article.loadAll(JSON.parse(localStorage.rawData));
     //TODO: What method do we call to render the index page?
     articleView.initIndexPage(localStorage.rawData);
   } else {
+    // TODO: When we don't already have the rawData,
+    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     $.getJSON('/data/hackerIpsum.json')
       .then(
-        // success callback
         function(data){
-          console.log(data);
-          Article.loadAll(data);
           Article.all.forEach(function(newArticleObject){
             $('#articles').append(newArticleObject.toHtml());
-            localStorage.setItem('articles: ', JSON.stringify(Article.all));
-            // articleView.initIndexPage();
           })
+          // cache it in localStorage so we can skip the server call next time,
+          localStorage.setItem('rawData', JSON.stringify(Article.all));
+          // then load all the data into Article.all with the .loadAll function above,
+          // and then render the index page.
+          Article.loadAll(data);
         },
         function(err) {
           console.error(err);
         }
       );
-    // TODO: When we don't already have the rawData,
-    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
 
-    // cache it in localStorage so we can skip the server call next time,
-    // then load all the data into Article.all with the .loadAll function above,
-    // and then render the index page.
   }
 }
